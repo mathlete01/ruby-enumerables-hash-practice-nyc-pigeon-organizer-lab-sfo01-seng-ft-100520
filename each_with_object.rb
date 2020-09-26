a8ec813a0226d3d52c6451792ad0dfff46522198
Matthew Sallin <msallin@gmail.com>
2020-09-26 21:12:54 +0000

    Automatically backed up by Learn

--- each_with_object.rb
+++ each_with_object.rb
@@ -1,7 +1,7 @@
 =begin
 This method iterates over each key-value pair and
 creates a new hash with the object passed into #each_with_object.
-So, the block variable newHash gets assigned to an empty hash, 
+So, the block variable newHash gets assigned to an empty hash,
 which is then built up according to set rules.
 =end
 
@@ -9,7 +9,7 @@ def transform_hash()
   hash = { a: 1, b: 2 }
   puts "hash = #{hash}"
   hash.each_with_object({}) do |(key, value), newHash|
-   newHash[key] = value + 1
+   newHash{key} = value + 1
    puts "newHash = #{newHash}"
   end
 end
